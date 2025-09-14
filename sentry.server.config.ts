f2976434a45fd52834838ce3afe46ebf6d97f511
parfaitBashombe <parfaitbashome@gmail.com>
2025-09-14 07:06:44 +0300

    ft(sentry):add

---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://985b7b38879695e1fc3895d046caaf53@o4510015879380992.ingest.us.sentry.io/4510015904415744",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
